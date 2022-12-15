05be7f00177a0b516411dd37274aee246c7d93ec
Darshan Rangoliya <darshanr.tagline@gmail.com>
2022-12-15 17:39:34 +0530

    question 6 added

---
+++ sixth.py
@@ -0,0 +1,16 @@
+
+string = "PQRQRQRQRQRQ" 
+iter(string)
+substring = "QRQ"
+
+
+total_str_length=len(string)
+total_sub_str_length = len(substring)
+
+cnt = 0
+for i in range(0,(total_str_length-2)):
+    tmp = string[i:i+total_sub_str_length]
+    if tmp == substring:
+        cnt+=1
+    
+print(cnt)
