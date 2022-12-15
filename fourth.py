a8792537bd1c2dda3a25270fa5f65db4ba0144bc
Darshan Rangoliya <darshanr.tagline@gmail.com>
2022-12-15 12:53:26 +0530

    concept of memoization added as well as another sum method added

--- fourth.py
+++ fourth.py
@@ -4,18 +4,49 @@ def fibo_fun(n):
     else:
         return fibo_fun(n-1)+fibo_fun(n-2)
 
+
 print(fibo_fun(8))
 
+# to reduece time complexity
 
-Numbers = [23, 44, 5, 67, 1, 1, 2, 4, 5]
-sum = 0
-def rec_sum(index):
-    global sum
-    if index == (len(Numbers)):
-        return sum
+num = int(input('Enter number :'))
+
+cache_store = {1: 1, 2: 1}
+
+
+def fibo_with_memorization(n):
+    if n in cache_store:
+        return cache_store[n]
     else:
-        sum+=Numbers[index]
-        return rec_sum(index+1)
+        cache_store[n] = fibo_with_memorization(n-1) + fibo_with_memorization(n-2)
+        return cache_store[n]
 
-rec_sum(0)
-print(sum)
+
+for i in range(1, num+1):
+    if i == num:
+        print(fibo_with_memorization(i))
+
+numbers = [23, 44, 5, 67, 1, 1, 2, 4, 5]
+
+# sum = 0
+# def rec_sum(index):
+#     global sum
+#     if index == (len(numbers)):
+#         return sum
+#     else:
+#         sum+=numbers[index]
+#         return rec_sum(index+1)
+
+# rec_sum(0)
+# print(sum)
+
+# another way
+
+def rec_sum(index):
+    if index < 0:
+        return 0
+    else:
+        return numbers[index]+rec_sum(index-1)
+
+
+print(rec_sum(len(numbers)-1))
