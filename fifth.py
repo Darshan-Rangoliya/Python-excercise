b10f867b88ea8d0c643feb6285ef9486b3652297
Darshan Rangoliya <darshanr.tagline@gmail.com>
2022-12-15 18:39:56 +0530

    new method with combinations (sum pair )

--- fifth.py
+++ fifth.py
@@ -1,19 +1,33 @@
 numbers = [9, 4, 8, 10, 2, 4, 8, 3, 14, 4, 8]
 
-print(numbers.reverse())
+# print(numbers.reverse())
 
 n = int(input('Enter the number of which you want sum from list :'))
 
+# sum_list = []
+
+# for number in numbers:
+#     for i in range(len(numbers)):
+#         if number + numbers[i] == n:
+#             tmp = [number,numbers[i]]
+#             if tmp not in sum_list:
+#                 tmp.reverse()
+#                 if tmp not in sum_list:
+#                     sum_list.append(tmp)
+
+
+# print(sum_list)
+
+from itertools import combinations
+
+pairs = set(combinations(numbers,2))
+
 sum_list = []
 
-for number in numbers:
-    for i in range(len(numbers)):
-        if number + numbers[i] == n:
-            tmp = [number,numbers[i]]
-            if tmp not in sum_list:
-                tmp.reverse()
-                if tmp not in sum_list:
-                    sum_list.append(tmp)
+for pair in pairs:
+    if sum(pair) == n:
+        sum_list.append(pair)
 
+final_list = set((tuple(sorted(value)) for value in sum_list))
 
-print(sum_list)
+print(final_list)
