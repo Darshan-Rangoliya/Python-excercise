1137396db03adce3ec783eb788a44f1d96c1d052
Darshan Rangoliya <darshanr.tagline@gmail.com>
2022-12-15 12:53:26 +0530

    concept of memoization added

--- fourth.py
+++ fourth.py
@@ -6,15 +6,29 @@ def fibo_fun(n):
 
 print(fibo_fun(8))
 
+# to reduece time complexity
 
-Numbers = [23, 44, 5, 67, 1, 1, 2, 4, 5]
+cache_store = {0:1,1:1}
+
+def fibo_fun_witch_cache(n):
+    if n in {0,1}:
+        return cache_store[n]
+    else:
+        cache_store[n] = fibo_fun_witch_cache(n-1)+fibo_fun_witch_cache(n-2)
+        return cache_store[n]
+
+fibo_nums = [fibo_fun_witch_cache(n) for n in range(8)]
+
+print(fibo_nums[-1])
+
+numbers = [23, 44, 5, 67, 1, 1, 2, 4, 5]
 sum = 0
 def rec_sum(index):
     global sum
-    if index == (len(Numbers)):
+    if index == (len(numbers)):
         return sum
     else:
-        sum+=Numbers[index]
+        sum+=numbers[index]
         return rec_sum(index+1)
 
 rec_sum(0)
