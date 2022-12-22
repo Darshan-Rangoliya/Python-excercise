42bd1032c5f714a9704f82c84925dd99145e114c
Darshan Rangoliya <darshanr.tagline@gmail.com>
2022-12-22 14:07:35 +0530

    ninth question modified

--- nine1.py
+++ nine1.py
@@ -10,29 +10,27 @@ end_time = datetime.strptime(end_time,'%d-%m-%Y %H:%M:%S')
 # start_time = datetime.strptime('18-12-2022 5:30:00','%d-%m-%Y %H:%M:%S')
 # end_time = datetime.strptime('19-12-2022 16:00:00','%d-%m-%Y %H:%M:%S')
 
-
+# here we are getting the diff between strat date and end date so after geting the night einterval we can delete that interval from this diff
 date_diff =  end_time-start_time
 
+# here we created start and end interval of two dates
 start_end_interval = p.closed(start_time,end_time)
 
 night_dates = []
 
+# here we are finding night intervals in list till end date
 while start_time < end_time+timedelta(days=1):
     night_dates.append(p.closed(datetime.combine(start_time,time()),datetime.combine(start_time,time())+timedelta(hours=6)))
     start_time+=timedelta(days=1)
 
-
-tot_night = []
-
-for nights in night_dates:
-    tot_night.append(start_end_interval&nights)
-
+# here we got only 
+tot_night = [start_end_interval&nights for nights in night_dates]
 
 tot_del_time = timedelta(0)
 
 for i in range(len(tot_night)):
     if tot_night[i].empty != True:
-        tmp = tot_night[i].upper- tot_night[i].lower
+        tmp = tot_night[i].upper - tot_night[i].lower
         tot_del_time+=tmp
 
 print('Total time between dates without night is {}'.format(date_diff-tot_del_time))
