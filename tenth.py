5f05c7253fef869254995f5722abd72feff4ab07
Darshan Rangoliya <darshanr.tagline@gmail.com>
2022-12-26 09:39:33 +0530

    diamond pattern with one loop

--- tenth.py
+++ tenth.py
@@ -1,134 +1,79 @@
-from math import ceil,floor
-# first pattern
+from math import ceil, floor
+n = int(input('Enter number: '))
 
+# first pattern
 # **_**
 # *___*
 # _____
 # *___*
 # **_**
-
-n = 9
-
-# for i in range(1,n):
-#     if i <= ceil(n/2):
-#         for k in range(floor(n/2)-i+1,0,-1):
-#             print('*',end='')
-#         for j in range(2*i-1):
-#             print('_',end='')
-#         for k in range(floor(n/2)-i+1,0,-1):
-#             print('*',end='')
-#         print()
-# for i in range(1,floor(n/2)+1):
-#     for k in range(i):
-#         print('*',end='')
-#     for j in range(n-(i*2)):
-#         print('_',end='')
-#     for k in range(i):
-#         print('*',end='')
-#     print()
-
-
-for index in range(0,n):
-    if index <= floor(n/2):
-        print('*'*floor((n/2)-index),end='')
-        print('_'*round(index*2+1),end='')
-        print('*'*floor((n/2)-index),end='')
-    else:
-        print('*'*floor(index%(n/2)+1),end='')
-        print('_'*floor((n%index)*2-1),end='')
-        print('*'*floor(index%(n/2)+1),end='')
+for index in range(0, n):
+    star = abs(floor((n/2) - index)) % (n-1)
+    space = n - 2*(abs(floor((n/2) - index)) % (n-1))
+    print('*'*star, end='')
+    print('_'*space, end='')
+    print('*'*star, end='')
     print()
-
-# print(5%3*2-1)
 
 print()
 
 # second pattern
-
 # __*__
 # _***_
 # *****
 # _***_
 # __*__
-
-# for i in range(1,n):
-#     if i <= ceil(n/2):
-#         for k in range(floor(n/2)-i+1,0,-1):
-#             print('_',end='')
-#         for j in range(2*i-1):
-#             print('*',end='')
-#         for k in range(floor(n/2)-i+1,0,-1):
-#             print('_',end='')
-#         print()
-# for i in range(1,floor(n/2)+1):
-#     for k in range(i):
-#         print('_',end='')
-#     for j in range(n-(i*2)):
-#         print('*',end='')
-#     for k in range(i):
-#         print('_',end='')
-#     print()
-
-for index in range(0,n):
-    if index <= floor(n/2):
-        print('_'*floor((n/2)-index),end='')
-        print('*'*round(index*2+1),end='')
-        print('_'*floor((n/2)-index),end='')
-    else:
-        print('_'*floor(index%(n/2)+1),end='')
-        print('*'*floor((n%index)*2-1),end='')
-        print('_'*floor(index%(n/2)+1),end='')
+for index in range(0, n):
+    star = abs(floor((n/2) - index)) % (n-1)
+    space = n - 2*(abs(floor((n/2) - index)) % (n-1))
+    print('_'*star, end='')
+    print('*'*space, end='')
+    print('_'*star, end='')
     print()
 
 print()
 
 # third pattern
-
 # *
 # **
 # *_*
 # *__*
 # *****
-
-for i in range(0,n):
-    for j in range(0,i+1):
-        if j==0 or i==n-1 or j==i:
-            print('*',end='')
+for i in range(0, n):
+    for j in range(0, i+1):
+        if j == 0 or i == n-1 or j == i:
+            print('*', end='')
         else:
-            print('_',end='')
+            print('_', end='')
     print()
 
 print()
 
 # fourth pattern
-
 # *****
 # *___*
 # *___*
 # *___*
 # *****
-
-for i in range(0,n):
-    for j in range(0,n):
-        if j==0 or i==n-1 or i==0 or j==n-1:
-            print('*',end='')
+for i in range(0, n):
+    for j in range(0, n):
+        if j == 0 or i == n-1 or i == 0 or j == n-1:
+            print('*', end='')
         else:
-            print('_',end='')
+            print('_', end='')
     print()
 
 print()
 
 # fifth pattern
-
 # 1
 # 2 3
 # 4 5 6
 # 7 8 9 10
 # 11 12 13 14 15
-
 cnt = 0
-for i in range(1,n+1):
-    for j in range(1,i+1):
-        cnt+=1
-        print(str(cnt)+' ',end='')
+for i in range(1, n+1):
+    for j in range(1, i+1):
+        cnt += 1
+        print(str(cnt)+' ', end='')
     print()
